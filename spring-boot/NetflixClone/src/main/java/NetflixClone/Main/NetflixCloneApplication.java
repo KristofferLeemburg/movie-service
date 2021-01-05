813043d4ed857044b500c3fe9e7a111bb349206b
Irene van Dijk <irene.a.vandijk@gmail.com>
2021-01-05 11:31:49 +0100

    maven project Netflix

---
+++ spring-boot/NetflixClone/src/main/java/NetflixClone/Main/NetflixCloneApplication.java
@@ -0,0 +1,13 @@
+package NetflixClone.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneApplication.class, args);
+	}
+
+}
